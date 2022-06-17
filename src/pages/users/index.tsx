7fe42983cb990aa2b609d9b3aa205c0a056a6217
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-17 10:33:07 -0300

    feat: criando página da lista de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,141 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+  Td,
+  Text,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="small"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="32">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Felipe Bastos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      felipe.bastos.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>15 de Junho, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="whiteAlpha"
+                    color="gray.900"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Felipe Bastos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      felipe.bastos.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>15 de Junho, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="whiteAlpha"
+                    color="gray.900"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Felipe Bastos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      felipe.bastos.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>15 de Junho, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="whiteAlpha"
+                    color="gray.900"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
