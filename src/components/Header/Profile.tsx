71b7f4f55c234d363cbbed40571c50bb6741a904
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-06-19 19:58:45 -0300

    style: trabalhando com a responsividade do Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Text, Flex } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Felipe Bastos</Text>
-        <Text color="gray.300" fontSize="small">
-          felipe.bastos.dev@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Felipe Bastos</Text>
+          <Text color="gray.300" fontSize="small">
+            felipe.bastos.dev@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
