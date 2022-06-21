6e58c20e10060cbafac1f4f0ce5c99d9bb444bb8
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-21 13:59:37 -0300

    feat: criando o formulário de signIn com react-hook-form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const error = formState.errors;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,17 +28,29 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" />
-          <Input name="password" type="password" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register("password")}
+          />
         </Stack>
         <Button
           type="submit"
           mt="6"
           colorScheme="pink"
           size="lg"
-          >
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
