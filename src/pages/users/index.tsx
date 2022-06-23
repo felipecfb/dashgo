aaa6390ba35a87b87e91acde7fe35388315e9812
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-23 14:21:32 -0300

    feat: adicionando axios no lugar do fetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,11 +22,11 @@ import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("hhtp://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
+    const { data } = await api("users");
 
     const users = data.users.map(user => {
       return {
@@ -63,6 +63,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
             </Heading>
 
             <Link href="/users/create" passHref>
