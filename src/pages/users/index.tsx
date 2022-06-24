7e84ec06844d6dd1a07faf7a637fecb1d18dd957
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-24 10:51:39 -0300

    feat: adicionando prefetch do usuário pelo ID com efeito hover

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,15 +14,18 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -32,6 +35,16 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10,
+    })
+  }
 
   return (
     <Box>
@@ -49,7 +62,7 @@ export default function UserList() {
               )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -59,7 +72,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -91,10 +104,14 @@ export default function UserList() {
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link color="purple.400" onMouseEnter={()=> handlePrefetchUser(user.id)}>
+                            <Text fontWeight="bold">
+                              {user.name}
+                            </Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300">
-                            {user.email}
-                          </Text>
+                              {user.email}
+                            </Text>
                         </Box>
                       </Td>
                       {isWideVersion && <Td>{user.created_at}</Td>}
