faab91d9ce5e42f4901f6123d39373b477c5883a
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-27 09:58:30 -0300

    feat: adicionando yup e configurando validação dos inputs de criação e de login de usuário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from "../components/Form/Input";
 
 type SignInFormData = {
@@ -7,8 +9,15 @@ type SignInFormData = {
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function Home() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
 
   const error = formState.errors;
 
@@ -36,12 +45,14 @@ export default function Home() {
             type="email"
             label="E-mail"
             {...register("email")}
+            error={error.email}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
             {...register("password")}
+            error={error.password}
           />
         </Stack>
         <Button
